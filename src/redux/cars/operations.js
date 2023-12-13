284598a862040392c580b249c2a459e05b582cff
andriy <evicandrij@gmail.com>
2023-12-13 20:37:49 +0200

    fix

--- src/redux/cars/operations.js
+++ src/redux/cars/operations.js
@@ -4,11 +4,19 @@ import axios from 'axios';
 axios.defaults.baseURL =
   'https://657047b009586eff66410b54.mockapi.io/advert/cars';
 
-export const fetchCar = createAsyncThunk('/', async (limit) => {
-  const res = await axios.get(`/?page=1&limit=${limit}`);
-  return res.data;
+export const fetchCar = createAsyncThunk('/', async (limit,{ rejectWithValue }) => {
+      try {
+      const res = await axios.get(`/?page=1&limit=${limit}`);
+      return res.data;  
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
 });
-export const fetchCarAll = createAsyncThunk('/all', async () => {
-  const res = await axios.get(`/`);
-  return res.data;
+export const fetchCarAll = createAsyncThunk('/all', async ({ rejectWithValue }) => {
+  try {
+    const res = await axios.get(`/`);
+    return res.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
 });
